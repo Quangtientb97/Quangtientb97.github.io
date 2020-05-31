ba3721f7c57157e372e97b4e00b9b963573915ae
Quangtien <Quangtientb97@gmail.com>
2020-05-31 18:16:18 +0700

    why3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
