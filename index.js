8a22619bc536b0d3d6be3d35548d88a2d02b2aaa
Quangtien <Quangtientb97@gmail.com>
2020-05-31 18:11:47 +0700

    why2

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ var server = require("http").Server(app);
 var io = require('socket.io')(server);
 server.listen(8080);
 
+var conn = mysql.createConnection({
+      host    : 'bsywt8xxwecvkrkhr23n-mysql.services.clever-cloud.com',
+      user    : 'usu17j382kwvccl2',
+      password: 'cujVo7AgzcaJf671v3bc',
+      database: 'bsywt8xxwecvkrkhr23n',
+});
+
 io.on('connection', function (socket) { //Bắt sự kiện một client kết nối đến server
   console.log(socket.id + " connected");
   socket.join("control");
+  conn.connect(function (err){
+    if (err) throw err.stack;
+    console.log("ket noi thanh cong");
+  });
   socket.on('mode', function(data){
   	console.log("nhan mode");
   	io.sockets.in('control').emit('control_mode', data);
