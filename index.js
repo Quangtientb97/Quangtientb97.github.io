4ae6f1a14927d88a5a1153a9565ea541d5b6a76c
Quangtien <Quangtientb97@gmail.com>
2020-06-02 16:25:49 +0700

    adcccda

--- index.js
+++ index.js
@@ -123,57 +123,46 @@ io.on('connection', function (socket) { //Bắt sự kiện một client kết n
   conn.connect(function (err){
     //if (err) throw err.stack;
     console.log("ket noi thanh cong");
-    /*let sql0 = `CREATE TABLE IF NOT EXISTS device1_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
+    let sql0 = `CREATE TABLE IF NOT EXISTS device1_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
       //console.log(sql0);
     conn.query(sql0, function (err) {
         //if (err) throw err;
         console.log('Tao bang thanh cong');
-    });*/
-    /*con.query('CREATE TABLE IF NOT EXISTS device3_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB', function(err,result, fields){
-        con.on('error',function(err){
-          console.log('mysql error',err);
-        });
-        console.log('Tao bang device3_log thanh cong');
-    });*/
-
-
-    socket.on('mode', function(data){
-    	console.log("nhan mode");
-    	io.sockets.in('control').emit('control_mode', data);
     });
-
-
-    socket.on('client-gui-user', function(data){
-    	console.log("nhan client-gui-user");
-    });  
-
-
-    socket.on('update_data', function (data) { //thông số động cơ
-      //socket.broadcast.emit('news', data); 
-      console.log("nhan update");
-      var data_json = JSON.stringify(data)
-      console.log(".");
-      console.log(data);
-      console.log("devide id: " + data.device_id);
-      console.log("tocdo: " + data.tocdo);
-      console.log("chieuquay: " + data.chieuquay);
-      conn.connect(function (err){
-      //if (err) throw err.stack;
-        //nếu thành công
-        let sql0 = `CREATE TABLE IF NOT EXISTS device${data.device_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,ThoiGian DATETIME DEFAULT CURTIME(), chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
-        //console.log(sql0);
-        conn.query(sql0, function (err) {
-            //if (err) throw err;
-            //console.log('Tao bang thanh cong');
-        });
-        let sql1 = `INSERT INTO device${data.device_id}_log(chieuquay, tocdo) values (  \'${data.chieuquay}\', \'${data.tocdo}\')` ;
-        //console.log(sql1);
-        conn.query(sql1, function (err) {
-            //if (err) throw err;
-            //console.log('Thay doi thanh cong');
-        });
+  });
+  socket.on('mode', function(data){
+  	console.log("nhan mode");
+  	io.sockets.in('control').emit('control_mode', data);
+  });
+  socket.on('client-gui-user', function(data){
+  	console.log("nhan client-gui-user");
+  });  
+  socket.on('update_data', function (data) { //thông số động cơ
+    //socket.broadcast.emit('news', data); 
+    console.log("nhan update");
+    var data_json = JSON.stringify(data)
+    console.log(".");
+    console.log(data);
+    console.log("devide id: " + data.device_id);
+    console.log("tocdo: " + data.tocdo);
+    console.log("chieuquay: " + data.chieuquay);
+    conn.connect(function (err){
+    //if (err) throw err.stack;
+      //nếu thành công
+      let sql0 = `CREATE TABLE IF NOT EXISTS device${data.device_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,ThoiGian DATETIME DEFAULT CURTIME(), chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
+      //console.log(sql0);
+      conn.query(sql0, function (err) {
+          //if (err) throw err;
+          //console.log('Tao bang thanh cong');
+      });
+      let sql1 = `INSERT INTO device${data.device_id}_log(chieuquay, tocdo) values (  \'${data.chieuquay}\', \'${data.tocdo}\')` ;
+      //console.log(sql1);
+      conn.query(sql1, function (err) {
+          //if (err) throw err;
+          //console.log('Thay doi thanh cong');
       });
     });
+  });
 
   //app
 
