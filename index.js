7315068b1a748b50466d1a3ddde1d39be4045b21
Quangtien <Quangtientb97@gmail.com>
2020-06-09 17:45:08 +0700

    abc

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ io.sockets.on('connection', function(socket){
 		var json = `{"rota":${data.rota},"mode":${data.mode}}`;	
 		const obj = JSON.parse(json);
 
-		io.sockets.emit('send-motor', obj); // gửi cho tất cả client
+		io.to(`${device_id}`).emit('send-motor', obj);
 	});	
 	socket.on('disconnect', function(data){
 		console.log(socket.id + 'disconnect');
